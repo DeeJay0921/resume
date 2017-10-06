07757e91ec637bdc53a811b4412190e80d6771c8
DeeJay <1018805743@qq.com>
2017-10-06 15:30:59 +0800

    structure

---
+++ src/js/index.js
@@ -0,0 +1,2 @@
+require('normalize.css')
+require('../less/index.less')
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var webpack  = require('webpack')
+var path = require('path')
+
+module.exports = {
+    entry: path.join(__dirname,'src/js/index.js'),
+    output: {
+        path: path.join(__dirname,'public/js'),
+        filename: 'index.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: ['style-loader','css-loader','less-loader']
+            }
+        ]
+
+    }
+}
